d6451d7f9222622931325597a9c6df2add63bc32
AvirukBasak <avirukbasak@gmail.com>
2022-10-31 15:16:14 +0530

    added placeholding commands

--- natas/hackall.sh
+++ natas/hackall.sh
@@ -8,3 +8,29 @@ source ./natas05/soln.sh
 source ./natas06/soln.sh
 source ./natas07/soln.sh
 source ./natas08/soln.sh
+source ./natas09/soln.sh
+# source ./natas10/soln.sh
+# source ./natas11/soln.sh
+# source ./natas12/soln.sh
+# source ./natas13/soln.sh
+# source ./natas14/soln.sh
+# source ./natas15/soln.sh
+# source ./natas16/soln.sh
+# source ./natas17/soln.sh
+# source ./natas18/soln.sh
+# source ./natas19/soln.sh
+# source ./natas20/soln.sh
+# source ./natas21/soln.sh
+# source ./natas22/soln.sh
+# source ./natas23/soln.sh
+# source ./natas24/soln.sh
+# source ./natas25/soln.sh
+# source ./natas26/soln.sh
+# source ./natas27/soln.sh
+# source ./natas28/soln.sh
+# source ./natas29/soln.sh
+# source ./natas30/soln.sh
+# source ./natas31/soln.sh
+# source ./natas32/soln.sh
+# source ./natas33/soln.sh
+# source ./natas34/soln.sh
