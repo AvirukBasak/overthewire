7c73ae94d49c0688381c48fb02816587943e28a2
AvirukBasak <avirukbasak@gmail.com>
2022-11-04 22:25:28 +0530

    natas11

--- natas/natas11/soln.sh
+++ natas/natas11/soln.sh
@@ -1,4 +1,4 @@
 #!/bin/echo Please source
 
-export natas12pass=$(curl -H "Cookie: $(python natas11/soln.py)" -s -u natas11:$natas11pass natas11.natas.labs.overthewire.org/ | grep "The password for natas12 is " | sed 's/The password for natas12 is //')
+export natas12pass=$(curl -H "Cookie: $(python natas11/soln.py)" -s -u natas11:$natas11pass natas11.natas.labs.overthewire.org/ | grep "The password for natas12 is " | sed 's/The password for natas12 is //' | sed 's/<br>//')
 echo natas12: $natas12pass
