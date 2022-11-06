beadec117d866e8b3a89460d86a9ee26d4938524
AvirukBasak <avirukbasak@gmail.com>
2022-11-06 17:49:04 +0530

    na

--- natas/natas15/soln.sh
+++ natas/natas15/soln.sh
@@ -1,4 +1,6 @@
 #!/bin/echo Please source
 
+# curl -s -v -u natas15:$natas15pass  -X POST -d "username=natas16" natas15.natas.labs.overthewire.org/\?debug
+
 export natas16pass=$()
 echo natas16: $natas16pass
